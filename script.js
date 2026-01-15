50c3b9e469b0395744529e1c07858e1aa0dd361f
oliveiraeliel990-beep <oliveiraeliel990@gmail.com>
2026-01-15 10:39:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Wonder Ultimate - Script básico
+// Arquivo preparado para expansão futura
+// Compatível com Google AdSense
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Wonder Ultimate carregado com sucesso");
+});
