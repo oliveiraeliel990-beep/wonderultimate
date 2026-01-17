813c89a19ffc087065d085893404ab3be363d00d
oliveiraeliel990-beep <oliveiraeliel990@gmail.com>
2026-01-17 17:41:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,12 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Wonder Ultimate carregado com sucesso");
+
+});
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu-lateral");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
 });
